99c86ddc26d9671f614a0785c9d49bcbd65b0276
mohmadit <159051823+mohmadit@users.noreply.github.com>
2024-08-11 00:09:51 +0300

    Updating code for task ID 165 and code ID 234 with type backend

--- cristianooooooooxz/devops_hfgjk_165_234_backend.php
+++ cristianooooooooxz/devops_hfgjk_165_234_backend.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-require 'config.php';
+session_start()
+require 'config.php'
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
 $email = $_POST['email'];
 $password = $_POST['password'];
 $role = $_POST['role'];
