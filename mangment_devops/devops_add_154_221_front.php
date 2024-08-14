07a44ac9e8a59da5a785c7bebe85fb8b401ceb79
mohmadit <159051823+mohmadit@users.noreply.github.com>
2024-08-14 16:06:23 +0300

    Updating code for task ID 154 and code ID 221 with type front

--- mangment_devops/devops_add_154_221_front.php
+++ mangment_devops/devops_add_154_221_front.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html lang="en"
+<!DOCTYPE html
+<html lang="en">
 <head>
-<meta charset="UTF-8"
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"
 <title>Hotel Room Booking</title>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="styleshee
 <style>
 body {
 background-color: #f8f9fa;
