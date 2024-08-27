964a6a10a4ed0bd7e8e9cafff3c7ee729139771d
mohmadit <159051823+mohmadit@users.noreply.github.com>
2024-08-27 20:47:35 +0300

    Updating code for task ID 174 and code ID 241 with type backend

--- biuld_project/devops_process_addd_174_241_backend.php
+++ biuld_project/devops_process_addd_174_241_backend.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
+$servername = "localhost
 $username = "root";
 $password = "";
 $dbname = "hotel_booking";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbn
 
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
