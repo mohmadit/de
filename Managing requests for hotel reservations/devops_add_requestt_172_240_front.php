da041db95c310db16da2361dc0e64cb8f6dd95e4
mohmadit <159051823+mohmadit@users.noreply.github.com>
2024-08-27 23:51:35 +0300

    Updating code for task ID 172 and code ID 240 with type front

--- Managing requests for hotel reservations/devops_add_requestt_172_240_front.php
+++ Managing requests for hotel reservations/devops_add_requestt_172_240_front.php
@@ -3,10 +3,10 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hotel Room Booking</title>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<style>
-body {
+<title>Hotel Room Booking</title
+<link hrefhttps://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<style
+body 
 background-color: #f8f9fa;
 }
 .container {
